ea69089f167922851f969694bd152048f53bc69e
michaelkora <michael.koras0@gmail.com>
2022-01-06 14:56:08 +0100

    ubuntu version changed

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#files= ls conda-forge_xtensor_files| wc -l
-#echo $files
-# >> state.txt
-echo $GITHUB_PAT | oras login https://ghcr.io -u MichaelKora --password-stdin
-
-oras push ghcr.io/${GITHUB_OWNER}/samples/artifact:1.0 \
-  ../../conda-forgeMichaelKora*
