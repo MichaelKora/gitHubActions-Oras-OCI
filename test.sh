2918f90644e650b5fc979285883030817d7058f7
michaelkora <michael.koras0@gmail.com>
2022-01-07 13:06:18 +0100

    testing bash script

---
+++ test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#my_array=($(ls -d */))
+my_array=($(ls conda-forge_xtensor_files/*.bz2))
+
+for i in "${my_array[@]}"
+do
+echo "$i"
+oras push ghcr.io/${{ env.GITHUB_OWNER }}/samples/xtensor:1.0 ./$i:application/octet-stream
+done
