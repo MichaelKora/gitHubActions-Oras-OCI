55be42411d620813cb3f4d5d64d59d9d7ddd170e
michaelkora <michael.koras0@gmail.com>
2022-01-07 14:20:27 +0100

    second time trying oras

--- upload-files.sh
+++ upload-files.sh
@@ -4,5 +4,5 @@ my_array=($( ls ./*.bz2 ) )
 for i in "${my_array[@]}"
 do
 echo "$i"
-oras push ghcr.io/${{ env.GITHUB_OWNER }}/samples/xtensor:1.0 ./$i:application/octet-stream
+oras push ghcr.io/${{ env.GITHUB_OWNER }}/samples/xtensor:1.0 $i:application/octet-stream
 done
