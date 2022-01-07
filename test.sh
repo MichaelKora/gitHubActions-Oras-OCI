b74441968e587ededbe48ce08ea53cf0014d4b9b
michaelkora <michael.koras0@gmail.com>
2022-01-07 13:09:08 +0100

    testing bash script 2

--- test.sh
+++ test.sh
@@ -5,5 +5,5 @@ my_array=($(ls conda-forge_xtensor_files/*.bz2))
 for i in "${my_array[@]}"
 do
 echo "$i"
-oras push ghcr.io/${{ env.GITHUB_OWNER }}/samples/xtensor:1.0 ./$i:application/octet-stream
+#oras push ghcr.io/${{ env.GITHUB_OWNER }}/samples/xtensor:1.0 ./$i:application/octet-stream
 done
