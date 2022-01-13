d66a335c578d2e366cddc92d10ff4bf34d83510c
michaelkora <michael.koras0@gmail.com>
2022-01-13 15:09:42 +0100

    using sh script to upbload

--- upload-files.sh
+++ upload-files.sh
@@ -1,9 +1,8 @@
-#  #!/usr/bin/env bash
 #!/bin/bash
-my_array=($( ls ./*.bz2 ) )
+owner=$1
+my_array=($( ls ./conda-forge_xtensor_files/*.bz2 ) )
 
 for i in "${my_array[@]}"
 do
-echo "$i"
-oras push ghcr.io/${{ env.GITHUB_OWNER }}/samples/xtensor:1.0 "$i":application/octet-stream
+oras push ghcr.io/$owner/samples/xtensor:1.0 "$i":application/octet-stream
 done
