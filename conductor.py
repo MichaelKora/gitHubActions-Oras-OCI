7b5cb41d6e7aef12357bbc65077431f0d4e0d6d5
michaelkora <michael.koras0@gmail.com>
2022-01-19 14:48:01 +0100

    trying to store one repoda/subdir

--- conductor.py
+++ conductor.py
@@ -3,6 +3,8 @@ import urllib.request
 import sys
 import subprocess
 import logging
+import os
+from datetime import datetime
 
 import urllib.request
 from functions import *
@@ -11,7 +13,7 @@ from functions import *
 owner = sys.argv[1]
 
 #shell script to create the needed file structure for conda_index
-subprocess.run("chmod +x ./prep_conda_index.sh", shell=True )
+subprocess.run("chmod +x ./conda_index_create.sh", shell=True )
 
 #shell script that actually run the conda index command
 subprocess.run("chmod +x ./run_conda_index.sh", shell=True )
@@ -27,6 +29,8 @@ last_link = ""
 with open("input_file.json", "r") as read_file:
     input_data = json.load(read_file)
 
+subprocess.run("mkdir temp_dir", shell=True)
+
 #go through entire set of entries in the input_data.json file
 for entry in input_data:
     chanel = entry["chanel"]
@@ -41,29 +45,22 @@ for entry in input_data:
 
     #run conda script to move the tar.bz2 file in the proper place
     #and then run the "conda index" to produce the repodata.json
-    subprocess.run("./prep_conda_index.sh", shell=True)
+#2    if (print (os.path.isdir(f"temp_dir/{subdir}")) == True):
+#2       subprocess.run(f"./conda_index_create.sh {subdir}", shell=True)
 
-    #download every found packages (tar.bz2 files) of a specific subdir
+
     for pkg in found_packages:
+        #donwload package
         pkgLink = f"https://conda.anaconda.org/{chanel}/{subdir}/{pkg}"
         logging.warning(f"Downloading the tar.bz2 file from {pkgLink}")
+        if (os.path.isdir(f"./temp_dir/{subdir}" == False):
+            subprocess.run(f"mkdir ./temp_dir{subdir}", shell=True)
+        urllib.request.urlretrieve(pkgLink, f"./temp_dir/{subdir}/{pkg}")
+#        urllib.request.urlretrieve(pkgLink, filename=pkg)
 
-        urllib.request.urlretrieve(pkgLink, filename=pkg)
-
-        # change the name and adapt the tag
-#        len_pkg = len (pkgname)
-
+        #get name, version and hash (tag = version + hash(without .tar.bz2 extension))
         name, version, hash = pkg.rsplit('-', 2)
         tag = version + "-" + hash
-
-#        tag = pkg[len_pkg: ]
-
-#        extension = ".tar.bz2"
-
-
-#len_tag= len(tag) - len (".tar.bz2")
-#tag_resized = tag [:len_tag]
-        # delete the ".tar.b2" part and replace all "_" with "-"
         tag_resized = tag.rpartition('.tar')[0]
         tag_resized = tag_resized.replace("_", "-")
 
@@ -78,22 +75,20 @@ for entry in input_data:
         logging.warning(f"Package <<{pkg}>> uploaded to: <<{upload_url}>>")
 
     #run "conda index" to update the repodata.json file
-    subprocess.run("./run_conda_index.sh", shell=True)
+    #subprocess.run("./run_conda_index.sh", shell=True)
 
     #upload the repodata.json file to the right url
-    json_url = f"ghcr.io/{owner}/samples/{subdir}:1.0"
-    push_json = f"oras push ghcr.io/{owner}/samples/{subdir}/repodata.json:1.0 ./temp_dir/noarch/repodata.json:application/vnd.unknown.layer.v1+txt"
-    logging.warning(f"Uploading repodata.json to <<{json_url}>>")
-    subprocess.run(push_json, shell=True)
-    logging.warning(f"File repodata.json upload to: <<{json_url}>>")
+#json_url = f"ghcr.io/{owner}/samples/{subdir}:1.0"
+#push_json = f"oras push ghcr.io/{owner}/samples/{subdir}/repodata.json:1.0 ./temp_dir/noarch/repodata.json:application/vnd.unknown.layer.v1+txt"
+logging.warning(f"Uploading all repodata.json files to...")
+#subprocess.run(push_json, shell=True)
+
+now = datetime.now()
+json_tag = dt_string = now.strftime("%d.%m.%Y-%H.%M.%S")
+subprocess.run(f"./deletefiles.sh {owner} {json_tag}", shell=True)
+
+logging.warning(f" All repodata.json files uploaded !!!>>")
 
 
     # delete bz2 and temp_dir
-    subprocess.run("./deletefiles.sh", shell=True)
-
-
-#/<name>/<arch>/repodata.json
-#und /<name>/<arch>/<package>
-#<name> -> irgendein Name
-#<arch> -> linux-64, osx-64, win-64, noarch
-#und <package> wäre jetzt z.b. xtensor-0.20.4-h1231312.tar.bz2
+#    subprocess.run("./deletefiles.sh", shell=True)
--- run_conda_index.sh
+++ run_conda_index.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
-cp ./*.bz2 ./temp_dir/noarch
-conda index temp_dir
+owner = $1
+tag = $2
+cd ./temp_dir
+#loc = noarch
+#repo = repodata.json
+
+for subdir in *
+#e.g. subdir = "linux-aarch64"
+do
+ conda index $subdir
+ cp ./temp_dir/$subdir/*.bz2 ./temp_dir/$subdir$/noarch
+ conda index $subdir
+ echo "uploading repodata <<./temp_dir/$subdir/noarch/repodata.json>> to ghcr.io/$owner/samples/$subdir/repodata.json:$tag"
+ oras push ghcr.io/$owner/samples/$subdir/repodata.json:$tag ./temp_dir/$subdir/noarch/repodata.json:application/vnd.unknown.layer.v1+txt
+ echo "repo data of $subdir uploaded"
+done
+
+#subdir = $1
+#cp ./$subdir/*.bz2 ./temp_dir/$subdir/noarch
+#conda index temp_dir/$subdir
+
+cd ..
