550c15e4e9b013700f240b85635483cc9c7aa858
michaelkora <michael.koras0@gmail.com>
2022-01-20 11:03:31 +0100

    test 5

--- run_conda_index.sh
+++ run_conda_index.sh
@@ -6,17 +6,17 @@ echo "tag is $2"
 cd ./temp_dir
 #loc = noarch
 #repo = repodata.json
+conda index
 
 for subdir in *
 #e.g. subdir = "linux-aarch64"
 do
- conda index
-
  #mv ./$subdir/*.bz2 ./$subdir/noarch
  #conda index ./$subdir
 
- #ls -al
- #ls -al ./$subdir
+ ls -al
+ ls -al ./$subdir
+ cat repodata.json
  #ls -al ./$Subdir/noarch
 
  echo "uploading repodata <<./temp_dir/$subdir/repodata.json>> to ghcr.io/$1/samples/$subdir/repodata.json:$2"
