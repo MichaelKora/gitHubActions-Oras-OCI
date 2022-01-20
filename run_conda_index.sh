a458281db04f7b21db44a588a11e2fba3310ae33
michaelkora <michael.koras0@gmail.com>
2022-01-20 11:06:15 +0100

    test 6

--- run_conda_index.sh
+++ run_conda_index.sh
@@ -13,14 +13,20 @@ for subdir in *
 do
  #mv ./$subdir/*.bz2 ./$subdir/noarch
  #conda index ./$subdir
-
+ echo "ls -al"
  ls -al
+
+ echo "ls -al ./$subdir"
  ls -al ./$subdir
+
+ echo "cating repodata.json"
  cat repodata.json
+ echo "cating ./$subdir/repodata.json"
+ cat ./$subdir/repodata.json
  #ls -al ./$Subdir/noarch
 
  echo "uploading repodata <<./temp_dir/$subdir/repodata.json>> to ghcr.io/$1/samples/$subdir/repodata.json:$2"
- oras push ghcr.io/$1/samples/$subdir/repodata.json:$2 ./$subdir/repodata.json:application/vnd.unknown.layer.v1+txt
+ oras push ghcr.io/$1/samples/$subdir/repodata.json:$2 ./$subdir/repodata.json:application/json
  echo "repo data of $subdir uploaded"
 done
 
