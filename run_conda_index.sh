554a85df4344966b771a1b9069f398f0fbd9bcf1
michaelkora <michael.koras0@gmail.com>
2022-01-20 10:13:48 +0100

    test

--- run_conda_index.sh
+++ run_conda_index.sh
@@ -12,6 +12,9 @@ do
  mv ./temp_dir/$subdir/*.bz2 ./temp_dir/$subdir/noarch
  conda index ./$subdir
  echo "uploading repodata <<./temp_dir/$subdir/noarch/repodata.json>> to ghcr.io/$owner/samples/$subdir/repodata.json:$tag"
+ ls -al
+ ls -al ./temp_dir/$subdir/
+ ls -al ./temp_dir/$subdir/noarch/
  oras push ghcr.io/$owner/samples/$subdir/repodata.json:$tag ./temp_dir/$subdir/noarch/repodata.json:application/vnd.unknown.layer.v1+txt
  echo "repo data of $subdir uploaded"
 done
