205cb75a7fd329208d3f9a9a8b5b9c466c05ffd2
michaelkora <michael.koras0@gmail.com>
2022-01-26 16:51:40 +0100

    blabla

--- conductor.py
+++ conductor.py
@@ -46,8 +46,8 @@ for entry in input_data_json:
     if repodata_exists:
         subprocess.run("pwd", shell=True)
         subprocess.run(f"ls -al", shell=True)
-        subprocess.run(f"ls -al ./temp_dir/{subdir}", shell=True)
         subprocess.run(f"ls -al ./temp_dir", shell=True)
+        subprocess.run(f"ls -al ./temp_dir/{subdir}", shell=True)
         with open(f"./temp_dir/{subdir}/repodata.json", "r") as read_file:
             current_repodata = json.load(read_file)
         for key_pkg in current_repodata["packages"]:
